b4b7d493d9f4b88995640eaf6367f13ee8fee215
TimEulink1 <tim.eulink22@gmail.com>
2021-11-26 10:24:31 +0100

    Base code

--- Arduino code/EasyLocker/src/main.cpp
+++ Arduino code/EasyLocker/src/main.cpp
@@ -1,9 +1,17 @@
 #include <Arduino.h>
+#define onboard 13
 
-void setup() {
+void setup()
+{
   //setup code
+  pinMode (onboard,OUTPUT);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
+  digitalWrite(onboard, LOW);
+  delay(1000);
+  digitalWrite(onboard, HIGH);
+  delay(1000);
 }
